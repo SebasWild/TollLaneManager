ec2eb880e15d1f2e1218b4ad340c14317e61d4c0
Sebastian Wild <wilds62@students.rowan.edu>
2016-02-19 23:24:02 -0500

    Added Lane object, with several fields. Added DependencyObjects for view to bind to. Added button to add/remove lanes (pretty dumb atm) Added log entries property to Lane for prototype.

---
+++ TollLaneManager/Converters/StatusToColorConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace TollLaneManager.Converters
+{
+    class StatusToColorConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            //Incoming value will be an int. An int with value 0 means green - all clear, 1 means yellow - some issues, and 2 means red - fatal issues with the toll lane.
+            int status = (int)value;
+            Brush result;                           //SolidColorBrush that represents the color of the Ellipses
+            switch(status)
+            {
+                case 0: result  = Brushes.Green;
+                    break;
+                case 1: result = Brushes.Yellow;
+                    break;
+                case 2: result = Brushes.Red;
+                    break;
+                default: result  = Brushes.Green;
+                    break;
+            }
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TollLaneManager/Entities/Lane.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TollLaneManager.Entities;
+
+
+namespace TollLaneManager.Entities
+{
+    /// <summary>
+    /// This object represents a traffic lane
+    /// </summary>
+    public class Lane
+    {
+        public string name { get; set; }
+        public int id { get; set; }
+        public int status { get; set; }
+
+        //In a production environment this should be some kind of "Event" object. Of course with a nice toString()
+        public ObservableCollection<String> logEntries { get; set; }       //Will hold all log entries. For prototyping purposes this is just a string.
+
+        //Example log strings:
+        string[] exEntries = { "A vehicle passed through the lane; RFID HIT; Customer billed", 
+                             "A vehilce passed through the lane; RFID MISS; License plate HIT; Customer billed",
+                             "ERROR; A car passed through the lane; RFID MISS; License plate MISS; Failed to bill customer!",
+                             "A vehicle passed through the lane; RFID HIT; Customer billed; Customer balance low."};            
+
+        public Lane(string name, int id)
+        {
+            this.name = name;
+            this.id = id;
+
+            logEntries = new ObservableCollection<string>();
+            populateLog();                                          //Put randon stuff in the log-for prototyping
+
+        }
+
+        private void populateLog()
+        {
+            Random rand = new Random();
+            int noEntries = rand.Next(0, 50);                       //How many random entries to add
+            for(int i = noEntries; i > 0; i--)
+            {
+                logEntries.Add(exEntries[rand.Next(0, 3)]);
+            }
+        }
+    }
+}
