0059af528127fcc9ce7fb09f9e8faee8a3eb20c2
Sebastian Wild <wilds62@students.rowan.edu>
2016-02-21 00:10:30 -0500

    Commit missing files

---
+++ TollLaneManager/Entities/Report.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TollLaneManager.Entities
+{
+    /// <summary>
+    /// A Report will hold a log of events over a specified time period.
+    /// 
+    /// Maybe even more detailed statistics.
+    /// </summary>
+    public class Report : Tabitem
+    {
+        public DateTime start { get; set; }
+        public DateTime end { get; set; }
+
+        public string name { get; set; }                                   //Name of the report
+
+        public ObservableCollection<String> logEntries { get; set; }       //Will hold all log entries for that time period.
+
+        public Report(String name, DateTime start, DateTime end, List<String> logEntries)
+        {
+            this.start = start;
+            this.end = end;
+            this.name = name;
+            this.logEntries = new ObservableCollection<string>(logEntries);
+        }
+    }
+}
