3f622184faaf36230042fa6dddb80b1bfbb76e2c
Sebastian Wild <wilds62@students.rowan.edu>
2016-04-10 12:30:27 -0400

    Added reference to OpenALPR. Added reference to System.Drawing Added example OpenALPR call (untested though)

--- TollLaneManager/Production Code/LicensePlateReader.cs
+++ TollLaneManager/Production Code/LicensePlateReader.cs
@@ -2,7 +2,7 @@ using AutomatedRoadTollingSystem;
 using System;
 using System.IO;
 using System.Windows.Media.Imaging;
-
+using openalprnet;
 namespace AutomatedRoadTollingSystem
 {
 	//This module will allow us to read the license plate numbers from the pictures and parse the information to a string.
@@ -15,5 +15,36 @@ namespace AutomatedRoadTollingSystem
             return "";
 				//Stub
 			}
+        /*
+         * Should work, havent tested it yet.
+         * This is how it will go...
+         * */
+        public void TestALPR()
+        {
+            var alpr = new AlprNet("us", "/path/to/openalpr.conf", "/path/to/runtime_data");
+            if (!alpr.IsLoaded())
+            {
+                Console.WriteLine("OpenAlpr failed to load!");
+                return;
+            }
+            // Optionally apply pattern matching for a particular region
+            alpr.DefaultRegion = "md";
+
+            var results = alpr.Recognize("/path/to/image.jpg");
+
+            for (int i = 0; i < results.Plates.Count; i++)
+            {
+                AlprPlateResultNet result = results.Plates[i];
+                Console.WriteLine("Plate {0}: {1} result(s)", i, result.TopNPlates.Count);
+                Console.WriteLine("  Processing Time: {0} msec(s)", result.ProcessingTimeMs);
+                foreach (var plate in result.TopNPlates)
+                {
+                    Console.WriteLine("  - {0}\t Confidence: {1}\tMatches Template: {2}", plate.Characters,
+                                      plate.OverallConfidence, plate.MatchesTemplate);
+                }
+            }
+
+        }
+
     }
 }
