2e7916635b0e54594689e196ffc273c237f30439
Sebastian Wild <wilds62@students.rowan.edu>
2016-04-10 12:01:37 -0400

    Fix Build error.

--- TollLaneManager/Production Code/LicensePlateReader.cs
+++ TollLaneManager/Production Code/LicensePlateReader.cs
@@ -1,6 +1,8 @@
 using AutomatedRoadTollingSystem;
 using System;
 using System.IO;
+using System.Windows.Media.Imaging;
+
 namespace AutomatedRoadTollingSystem
 {
 	//This module will allow us to read the license plate numbers from the pictures and parse the information to a string.
@@ -8,7 +10,7 @@ namespace AutomatedRoadTollingSystem
 	{
 		//Searches for a license plate in the Image an parses the plate info to a string.
 		//Returns a string representation of the license plate found.
-		public String readLicense(Image plate)
+		public String readLicense(BitmapImage plate)
 			{
             return "";
 				//Stub
