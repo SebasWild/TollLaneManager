ab6e40c9eb9d7549e15cdb5f7f457fc0f9f432f5
johnsone66 <erica.v.johnson@lmco.com>
2016-04-28 00:59:18 -0400

    changed folder

--- TollLaneManager/Common/DBActions.cs
+++ TollLaneManager/Common/DBActions.cs
@@ -193,7 +193,7 @@ namespace AutomatedRoadTollingSystem.Common
             } else
             {
                 var fileName = string.Format(@"{0}.txt", Guid.NewGuid());
-                System.IO.StreamWriter file = new System.IO.StreamWriter("c:\\TEMP\\" + fileName, true);
+                System.IO.StreamWriter file = new System.IO.StreamWriter("..\\..\\..\\BilltoCustomers" + fileName, true);
                 file.WriteLine("Plate Number: " + plateNo);
                 file.WriteLine("Toll Amount: " + fee);
                 file.Close();
